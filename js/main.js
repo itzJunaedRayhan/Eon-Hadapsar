17de80709c661220b7f7ecaf574cc63d7eb22654
Junaed Rayhan <junaedrayhan@gmail.com>
2021-07-07 01:09:02 +0600

    Investors Section Done

--- js/main.js
+++ js/main.js
@@ -84,3 +84,23 @@ function setActiveClass() {
   currentDot.classList.remove("active");
   navigationDots.children[currentSlide].classList.add("active");
 }
+
+
+
+
+
+//  Accordion Section
+jQuery(document).ready(function () {
+  const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
+  accordionItemHeaders.forEach(accordionItemHeader => {
+      accordionItemHeader.addEventListener("click", event => {
+          accordionItemHeader.classList.toggle("active");
+          const accordionItemBody = accordionItemHeader.nextElementSibling;
+          if (accordionItemHeader.classList.contains("active")) {
+              accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+          } else {
+              accordionItemBody.style.maxHeight = 0;
+          }
+      });
+  });
+})
